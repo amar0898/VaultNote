3e19278a95410d6ebfae66db6495a5ddccb575e2
amar0898 <amar.pinjore0898@gmail.com>
2024-12-12 17:41:35 -0500

    Implemented access restriction for REST API's which Admin Role only can access. Implemented it using URL based restriction in spring security configuration file

--- src/main/java/com/amardeep/VaultNote/security/SecurityConfig.java
+++ src/main/java/com/amardeep/VaultNote/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -23,11 +24,14 @@ import java.time.LocalDate;
 
 @Configuration
 @EnableWebSecurity
+//@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated());
         //http.formLogin(Customizer.withDefaults());
         http.csrf(csrf-> csrf.disable());
         http.httpBasic(Customizer.withDefaults());
