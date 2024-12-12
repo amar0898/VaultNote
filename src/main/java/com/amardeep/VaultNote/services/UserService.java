7710ced181e6c8bef6fe30585cb7a247121f237f
amar0898 <amar.pinjore0898@gmail.com>
2024-12-12 17:11:30 -0500

    created REST API's for Admin role of the app to get the all users, update any user role and get a specefic user based on userID

---
+++ src/main/java/com/amardeep/VaultNote/services/UserService.java
@@ -0,0 +1,14 @@
+package com.amardeep.VaultNote.services;
+
+import com.amardeep.VaultNote.dtos.UserDTO;
+import com.amardeep.VaultNote.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+}
