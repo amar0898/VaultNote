3e483a74ec111787e7006bc74f4ee8da46e94bc3
amar0898 <amar.pinjore0898@gmail.com>
2024-12-23 18:57:45 -0500

    Created navigation bar and the footer for the application

--- VaultNote-Frontend-React/vaultnote-frontend/src/App.js
+++ VaultNote-Frontend-React/vaultnote-frontend/src/App.js
@@ -4,14 +4,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import LandingPage from "./components/LandingPage";
 import './App.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <Router>
+    <Navbar />
     <Toaster position="bottom-center" reverseOrder={false} />
     <Routes>
     <Route path="/" element={<LandingPage />} />
     </Routes>
+    <Footer />
     </Router>
   );
 }
