2e4ed9077f941a3c16bb22e08aff4999acacfce1
amar0898 <amar.pinjore0898@gmail.com>
2024-12-24 17:47:35 -0500

    Added login and signup pages as a Route in the App.js to navigate to signup and login page on click

--- VaultNote-Frontend-React/vaultnote-frontend/src/App.js
+++ VaultNote-Frontend-React/vaultnote-frontend/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import LandingPage from "./components/LandingPage";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
 import './App.css';
 import Navbar from "./components/Navbar";
 import ContactPage from "./components/contactPage/ContactPage";
@@ -16,6 +18,8 @@ const App = () => {
     <Toaster position="bottom-center" reverseOrder={false} />
     <Routes>
     <Route path="/" element={<LandingPage />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
     <Route path="/contact" element={<ContactPage />} />
     <Route path="/about" element={<AboutPage />} />
     </Routes>
