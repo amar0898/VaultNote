397a996454ca26733ed6a19f1831b15b85d7edbf
amar0898 <amar.pinjore0898@gmail.com>
2024-12-30 18:40:45 -0500

    Added configurations to handle the CORS issue. Allowing the all requests from the react frontend url to our backend server

--- src/main/java/com/amardeep/VaultNote/security/SecurityConfig.java
+++ src/main/java/com/amardeep/VaultNote/security/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).ignoringRequestMatchers("/api/auth/public/**"));
+        //http.cors(Customizer.withDefaults());
         http.authorizeHttpRequests((requests) ->
                 requests.requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/csrf-token").permitAll()
